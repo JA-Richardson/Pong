a113ea97bb0c51f1951ac88281a9a9bcab534415
JA-Richardson <arichardson1103@gmail.com>
2022-05-17 23:21:27 +0100

    Initial bat setup
    Bat class created and inital sfml setup done

--- Pong.cpp
+++ Pong.cpp
@@ -1,22 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <sstream>
+#include <iostream>
+#include "Bat.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Pong!");
+    int score = 0;
+    int lives = 3;
+
+    Bat bat(1920 / 2, 1080 - 20);
+    sf::Text hud;
+    sf::Font font;
+    font.loadFromFile("Font/Positive System.otf");
+    hud.setFont(font);
+    hud.setCharacterSize(75);
+    hud.setPosition(20, 20);
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+        //input
+
+        //update
+
+        //draw
 
         window.clear();
-        window.draw(shape);
+       
         window.display();
     }
 
